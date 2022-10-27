f7ac48af987f9625b6e00092cad113d36bdb3c91
Kaden Nebeker <112510659+K-Nebeker@users.noreply.github.com>
2022-10-27 15:38:14 -0500

    Passed in Presenter into GameAction constructor

--- Presenter.java
+++ Presenter.java
@@ -16,7 +16,7 @@ public class Presenter {
         splash = new SplashScreen(); // load splash screen
         model = new Model(); // load model
         playerEntry = new PlayerEntry(this); // Load player entry screen with this as paramater
-
+        gameAction = new GameAction(this); // pass in presenter into game action
     }
 
     public void startApp() {
