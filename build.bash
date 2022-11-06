f3b5bd2af6752979d0310de084c87b014b728448
Justinh809 <justinh809@gmail.com>
2022-11-06 15:10:56 -0600

    Added GameAction to builds

--- build.bash
+++ build.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -u -e
-javac -cp .:postgresql-42.5.0.jar LaserTag.java Presenter.java Model.java SplashScreen.java PlayerEntry.java
+javac -cp .:postgresql-42.5.0.jar LaserTag.java Presenter.java GameAction.java Model.java SplashScreen.java PlayerEntry.java
 java -cp .:./postgresql-42.5.0.jar LaserTag
