e88e18a26dc2a082f2dea220a7775b0cf230c431
Kaden Nebeker <112510659+K-Nebeker@users.noreply.github.com>
2022-11-16 23:46:33 -0600

    Updated call to updateScoreboard().

--- Presenter.java
+++ Presenter.java
@@ -29,7 +29,7 @@ public class Presenter {
         splash.showSplash();
         // displaying splash for 3 seconds before we move on
         try {
-            Thread.sleep(1000); // I believe this makes the main thread wait 3 seconds
+            Thread.sleep(3000); // I believe this makes the main thread wait 3 seconds
         } catch (Exception e) {
             e.printStackTrace();
             System.exit(1);
@@ -43,7 +43,7 @@ public class Presenter {
         addPlayersToList(playerEntry.toBePassed);
         playerEntry.hidePlayerEntry();
         gameAction.showGameAction();
-        gameAction.updateScoreboard(model.acitveRedPlayers, model.acitveGreenPlayers);
+        gameAction.updateScoreboard();
         try {
 
             model.connection.close();
